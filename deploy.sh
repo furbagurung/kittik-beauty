b5b2f4300513085d5ebc69177e8b49dd9f369d84
furbagurung <foorvaa.grg@gmail.com>
2026-04-26 01:25:12 +0545

    Load Node 20 during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+nvm use 20
+
 cd /root/kittik-backend
+
+echo "Using Node:"
+node -v
+npm -v
 
 echo "Pulling latest code..."
 git pull origin main
