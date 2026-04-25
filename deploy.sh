c58cb86467121d6e8b040c9117a6f2dcf410be7d
furbagurung <foorvaa.grg@gmail.com>
2026-04-26 01:30:22 +0545

    Force Node 20 in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/root/.nvm"
 source "$NVM_DIR/nvm.sh"
 nvm use 20
+
+export PATH="/root/.nvm/versions/node/v20.20.2/bin:$PATH"
 
 cd /root/kittik-backend
 
 echo "Using Node:"
+which node
 node -v
+which npm
 npm -v
 
 echo "Pulling latest code..."
